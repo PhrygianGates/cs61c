e7b9a98f5f5b386de1f99b0ebe4a2ad5f0f4e17f
xzc <me@mydomain.com>
2021-05-19 23:21:37 +0800

    complete lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,17 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node *tortoise, *hare;
+    tortoise = hare = head;
+    while (1) {
+        if (hare == NULL || hare->next == NULL) {
+            return 0;
+        }
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise) {
+            return 1;
+        }
+    }
     return 0;
 }
