d8db01b6ec5a2d6cb28734ccf638373e35e1f20e
xzc <me@mydomain.com>
2021-06-03 17:32:20 +0800

    complete lab03

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -21,4 +21,14 @@ main:
     ecall # Exit
 
 factorial:
-    # YOUR CODE HERE
+    add t0 x0 a0
+    addi t1 x0 1
+    addi t2 x0 1
+loop:
+    blt t0 t2 break
+    mul t1 t1 t0
+    addi t0 t0 -1
+    jal x0 loop
+break:
+    add a0 x0 t1
+    jr ra
